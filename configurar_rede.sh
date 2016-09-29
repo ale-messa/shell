99e6746cdb1c652f97bcd2460bcf50243f9d1cf6
root <root@Lucimar.(none)>
2016-09-28 21:13:33 -0300

    Atualizando Shell

--- configurar_rede.sh
+++ configurar_rede.sh
@@ -8,15 +8,19 @@ touch /etc/network/interfaces # CRIA UM NOVO
 echo "auto eth0" >> /etc/network/interfaces #INSERE A PRIMEIRA LINHA DENTRO DO ARQUIVO INTERFACES
 echo "iface eth0 inet dhcp" >> /etc/network/interfaces #INSERE A SEGUNDA LINHA DENTRO DO ARQUIVO INTERFACES
 
-services networking restart #REINICIA O SERVIÇO DE REDE
+service networking restart #REINICIA O SERVIÇO DE REDE
 
-hostname_old = $HOSTNAME #DEFINE VALOR NA VARIAVEL COM O NOME ATUAL DO PC
+echo "Nome atual do computador"
+cat /etc/hostname
+
+echo "Informe o nome antigo do computador"
+read hostname_old
 
 echo "Informe um novo nome para PC" #EXIBE NA TELA
 read hostname_new #VARIAVEL
 
-sed -i "2s/$hostname_old/$hostname/" /etc/hosts #ALTERA A LINHA 2 DO ARQUIVO HOSTS >>>>> EXEMPLO: sed -i "2s'LINHA2'/$hostname_old'VALOR A SER PROCURADO'/$hostname'VALOR A SUBSTITUIR/" /etc/hosts'ARQUIVO'
-sed -i "1s/$hostname_old/$hostname/" /etc/hostname #ALTERA A LINHA 1 DO ARQUIVO HOSTNAME
+sed -i "2s/$hostname_old/$hostname_new/" /etc/hosts #ALTERA A LINHA 2 DO ARQUIVO HOSTS >>>>> EXEMPLO: sed -i "2s'LINHA2'/$hostname_old'VALOR A SER PROCURADO'/$hostname'VALOR A SUBSTITUIR/" /etc/hosts'ARQUIVO'
+sed -i "1s/$hostname_old/$hostname_new/" /etc/hostname #ALTERA A LINHA 1 DO ARQUIVO HOSTNAME
 
 
 ###########################################################
@@ -24,6 +28,6 @@ sed -i "1s/$hostname_old/$hostname/" /etc/hostname #ALTERA A LINHA 1 DO ARQUIVO 
 
 apt-get install openssh-server #INSTALA O SERVIÇO
 
-sed -i "25s/Porta: 22/Porta: 1000/" /etc/ssh/ssh.conf #ALTERA A LINHA 25 DO ARQUIVO SSH.CONF
+sed -i "5s/Port 22/Porta 1000/" /etc/ssh/sshd_config #ALTERA A LINHA 5 DO ARQUIVO SSHD_CONFIG
 
-services openssh restart #REINICIA O SERVIÇO DE SSH
+service openssh restart #REINICIA O SERVIÇO DE SSH
